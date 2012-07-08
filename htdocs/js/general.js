7539c43a99e978cd631abf6aa760479faaa334f4
Tiger Schwert <danny@badgame.net>
2012-07-08 01:24:04 +0100

    Happy halloween!

--- htdocs/js/general.js
+++ htdocs/js/general.js
@@ -1,3 +1,23 @@
+Badgame = this.Badgame || {};
+
+Badgame.SkeletonPrank = function() {
+	$(".skeleton").css("position", "fixed")
+		.css("display", "block")
+		.css("top", "-30px")
+		.css("left", "3000px");
+	$(".skeleton").animate({left: "-1000px"}, 5600, function() {
+		$(this).remove();
+	});
+}
+
 $(document).ready(function() {
 	$(".postrow[author-id='92']").find(".post").append("<div class='smalltext'>(This post may have been written by a retard.)</div>");
+	if (Modernizr.localstorage && !localStorage.getItem('skeleton.pranked')) {
+		// make sure it's loaded
+		$('body').append("<img class='skeleton' src='http://i.imgur.com/XqEel.png' style='display: none' />");
+		if (Math.random()*1000 < 5) {
+			Badgame.SkeletonPrank();
+			localStorage.setItem('skeleton.pranked', 'true');
+		}
+	}
 });
